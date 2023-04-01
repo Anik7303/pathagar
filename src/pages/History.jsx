cecf68b3c28c3d9f117776cdf0245f9f0b76b9f3
Anik Mohammad <anik7303@gmail.com>
2023-04-01 14:33:17 +0600

    working on history page

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -3,7 +3,11 @@ import React from "react";
 import "./History.css";
 
 function History() {
-  return <div className="container">History</div>;
+  return (
+    <div className="container">
+      <h2 className="section-title">How we came to be</h2>
+    </div>
+  );
 }
 
 export default History;
