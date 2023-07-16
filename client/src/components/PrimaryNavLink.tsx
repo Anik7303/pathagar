549db00332e76bf128c910d8bf008b9e092bb7a4
Anik Mohammad <anik7303@gmail.com>
2023-07-16 16:10:38 +0600

    header: change action button bg

--- client/src/components/PrimaryNavLink.tsx
+++ client/src/components/PrimaryNavLink.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import NavLink from "./NavLink";
 
 const PrimaryNavLink = styled(NavLink)`
-  background: #812511;
+  background: #009521;
   color: #f0f0f0;
   padding: 8px 16px;
   border-radius: 10px;
@@ -11,7 +11,7 @@ const PrimaryNavLink = styled(NavLink)`
   &:hover {
     text-decoration: none;
     transform: scale(1.05);
-    box-shadow: 2px 2px 10px 4px rgba(129, 37, 17, 0.3);
+    box-shadow: 2px 2px 10px 4px rgba(0, 149, 33, 0.3);
   }
 `;
 
